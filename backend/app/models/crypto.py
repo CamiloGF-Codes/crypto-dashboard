b4568b23af4320940906185137b7a3c6c1fd1c9a
Camilo <camilogonzalezfuentes1303@gmail.com>
2026-04-05 20:56:29 +0100

    feat: add SQLALchemy databse connection and Crypto model

---
+++ backend/app/models/crypto.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Crypto(Base):
+    __tablename__ = "cryptos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    symbol = Column(String, nullable=False)
+    name = Column(String, nullable=False)
+    price_usd = Column(Float, nullable=False)
+    market_cap = Column(Float)
+    recorded_at = Column(DateTime, server_default=func.now())
